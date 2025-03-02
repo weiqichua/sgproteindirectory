b6217499e4ae5d5eccfd610d4c8c2c35b147a5ec
weiqichua <143248754+weiqichua@users.noreply.github.com>
2025-03-02 22:25:19 +0800

    font changes

--- sgproteindirectory/src/app/page.tsx
+++ sgproteindirectory/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { inter } from "@/components/ui/fonts";
 
 export default function Home() {
   return (
     <div className = "flex flex-col">
-      <div className = "w-full min-h-20 bg-blue-500"> Navbar </div>
+      <div className = {` ${inter.className} antialiased text-3xl w-full min-h-24 bg-teal-500 fixed `}> Navbar </div>
+
      </div>
   );
 }
